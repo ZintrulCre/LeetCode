b762d9aaf0225ffac7ecd7bd16c5280c6396b07f
zintrulcre <zintrulcre@gmail.com>
2019-01-01 00:25:43 +1100

    448

--- Python/448.py
+++ Python/448.py
@@ -1,3 +1,14 @@
+# 1
+class Solution:
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return list(set(range(1, len(nums) + 1)) - set(nums))
+
+        
+# 2
 class Solution:
     def findDisappearedNumbers(self, nums):
         """
