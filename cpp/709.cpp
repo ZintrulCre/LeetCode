450f393af50bbdcb25956639049d743017e6c85a
zintrulcre <zintrulcre@gmail.com>
2019-06-24 21:59:43 +1000

    Remake

--- cpp/709.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    string toLowerCase(string str) {
-        for (int i = 0; i < str.size(); ++i)
-            str[i] = (str[i] >= 'A' && str[i] <= 'Z' ? str[i] += 'a' - 'A' : str[i]);
-        return str;
-    }
-};
